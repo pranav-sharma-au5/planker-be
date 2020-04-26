a787efb9d25570332dbe2b6e80bec71141e5de5a
pranav <pranav.pieces@gmail.com>
2020-04-26 12:32:44 +0530

    initial commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: "https://planker.netlify.app",
   credentials: true
 }))
 app.use(logger('dev'));
